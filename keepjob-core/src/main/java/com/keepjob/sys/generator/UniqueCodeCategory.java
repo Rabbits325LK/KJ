60a9dae602b22d9a744f2e0d7beda4b70b5669e0
Rabbits325LK <rabbits325@live.cn>
2016-01-27 18:21:25 +0800

    更新了keepjob-home 用于主页

--- keepjob-core/src/main/java/com/keepjob/sys/generator/UniqueCodeCategory.java
+++ keepjob-core/src/main/java/com/keepjob/sys/generator/UniqueCodeCategory.java
@@ -5,7 +5,9 @@ public enum UniqueCodeCategory {
 	ROLE_CODE("R"), //角色
 	STORE_CODE("STORE"),//店铺编码
 	CARTE_CODE("CARTE"),//菜谱编码
-	APP_ASSOCIATOR_CODE("AAC");
+	APP_ASSOCIATOR_CODE("AAC"),//app会员
+	KEEPJOB_MEMBERS_CODE("KMC"),//成员
+	KEEPJOB_WEB_MODEL_CODE("KWMC");//模版
 	private String value;
 	
 	private UniqueCodeCategory(String value){
