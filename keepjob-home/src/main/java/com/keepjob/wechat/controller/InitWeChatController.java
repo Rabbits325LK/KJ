d1b5e493042a5fbbe6b53dde96e33e51bcbf4d31
rabbits325 <rabbtis325@live.cn>
2016-10-17 16:21:17 +0800

    17

--- keepjob-home/src/main/java/com/keepjob/wechat/controller/InitWeChatController.java
+++ keepjob-home/src/main/java/com/keepjob/wechat/controller/InitWeChatController.java
@@ -1,16 +1,16 @@
 package com.keepjob.wechat.controller;
 
-import java.io.IOException;
+import com.keepjob.wechat.KeepJob;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.keepjob.wechat.KeepJob;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/weChat")
@@ -32,6 +32,12 @@ public class InitWeChatController {
 		response.getOutputStream().write(result.getBytes());
 		//ModelAndView model = new ModelAndView();
 		//return result;
+	}
+
+	@RequestMapping(value = "/index.html", method = RequestMethod.GET)
+	public String index(HttpSession session, HttpServletRequest request) {
+
+		return "wechat/index";
 	}
 }
 
