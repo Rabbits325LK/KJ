3b46e9eef0a1c3c18ed5925ef69cc546f7d163f3
Rabbits325LK <rabbits325@live.cn>
2017-01-08 15:58:53 +0800

    dsds

--- keepjob-home/src/main/java/com/keepjob/turnover/controller/TurnoverController.java
+++ keepjob-home/src/main/java/com/keepjob/turnover/controller/TurnoverController.java
@@ -1,13 +1,18 @@
 package com.keepjob.turnover.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.gson.Gson;
+import com.keepjob.turnover.Turnover;
 import com.keepjob.turnover.TurnoverHandler;
 
 @RequestMapping("/turnover")
@@ -16,11 +21,17 @@ public class TurnoverController {
 	
 	@Autowired
 	private TurnoverHandler turnoverHandler;
+	@Autowired
+	private Gson gson;
 	
 	@RequestMapping(value="/index.html", method = RequestMethod.GET)
-	public String indexView(HttpSession session, HttpServletRequest request) {
+    public String indexView(HttpSession session, HttpServletRequest request, Model model ) {
+		List<Turnover> turnovers = turnoverHandler.findTurnovers();
+		model.addAttribute("result", turnovers);
 		return "/turnover/index";
 	}
+	
+	
 	
 
 }
